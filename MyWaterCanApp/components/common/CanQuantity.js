28da3397be3fa0a1186bad9d4af89aa38785f92b
Nirmalkumar Selvaraj <nirmalkumar.s@accelfrontline.com>
2018-08-17 20:31:39 +0530

    refactor: unwanted classes and code removed

--- MyWaterCanApp/components/common/CanQuantity.js
+++ MyWaterCanApp/components/common/CanQuantity.js
@@ -6,18 +6,12 @@ import {
     View,
     Button
   } from 'react-native';
-  import { CheckBox } from 'react-native-elements'
-  import { connect } from 'react-redux';
-  import {counterIncrementer,counterDecrementer,fetchCounter} from '../../actions/Actions'
 export class CanQuantity extends React.Component {
 
 
     render() {
         return (
             <View>
-                {/*<Button title = '<' onPress = {this.props.decrement}></Button>
-                <Text>{this.props.count}</Text>
-                <Button title = '>' onPress = {this.props.increment}></Button>*/}
                 <Button title = '<' onPress = {this.props.decrement}></Button>
                 <Text style={styles.counterStyle}>{this.props.count}</Text>
                 <Button title = '>' onPress = {this.props.increment}></Button>
@@ -32,16 +26,3 @@ const styles = StyleSheet.create({
         alignItems: 'center'
     },
   });
-// const mapDispatchToProps = (dispatch) => {
-//     return {
-//         increment: () => dispatch(canIncrementer()),
-//         decrement: () => dispatch(canDecrementer()),
-//     };
-// };
-// function mapStateToProps(state) {
-//     console.log('Inital State', state);
-//     return {
-//         canCount: state.canCount
-//     };
-//   }
-// export default connect(mapStateToProps, mapDispatchToProps)(CanQuantity);
