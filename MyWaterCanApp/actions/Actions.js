1e77940452e84f6382520eef36047cee1d67c3f7
Nirmalkumar Selvaraj <nirmalkumar.s@accelfrontline.com>
2018-09-08 18:09:40 +0530

    fix: initial state is structure

--- MyWaterCanApp/actions/Actions.js
+++ MyWaterCanApp/actions/Actions.js
@@ -1,9 +1,9 @@
 
 
 
-export function initialLoad(value) {
-    return { type: 'INITIAL_STATE', value }
-}
+// export function initialLoad(value) {
+//     return { type: 'INITIAL_STATE', value }
+// }
 export function canIncrementer() {
     return { type: 'CAN_INCREMENT' }
 }
@@ -44,7 +44,7 @@ export function currentLocationUpdate(value) {
         return { type: 'NA'};
     }
 }
-/* export function initialLoad() {
+ export function initialLoad() {
     return (dispatch) => {
         fetch("http://ec2-13-127-170-233.ap-south-1.compute.amazonaws.com/api/v1/cans")
             .then((response) => {
@@ -64,7 +64,7 @@ export function currentLocationUpdate(value) {
                  dispatch(loadValue(null));// need to be removed 
             });
     };
-} */
+} 
 
 // export function bookNow() {
 //     return (dispatch) => {
--- MyWaterCanApp/reducers/CounterReducer.js
+++ MyWaterCanApp/reducers/CounterReducer.js
@@ -1,26 +1,26 @@
-  
-  export function masterReducer(state={}, action) {
+  const initialState = {
+    canQuantity: 25,
+    canUnit: 'Litres',
+    canCount: 2,
+    upperBound: 10,
+    lowerBound: 2,
+    emergency: true,
+    searchOnFocus: false,
+    currentUserLocation: {latitude:13.0517102 ,longitude:80.1899955}}
+
+  export function masterReducer(state=initialState, action) {
     switch(action.type) {
       case 'INITIAL_STATE':
-        return {
-          canQuantity: 25,
-          canUnit: 'Litres',
-          canCount: 2,
-          upperBound: 10,
-          lowerBound: 2,
-          emergency: true,
+        return  {
+          canQuantity: action.value.defaultCanOption.quantity,
+          canUnit: action.value.defaultCanOption.unit,
+          canCount: action.value.defaultCanOption.defaultNumber,
+          emergency: action.value.emergencyBooking,
+          upperBound: action.value.upperBound,
+          lowerBound: action.value.lowerBound,
           searchOnFocus: false,
           currentUserLocation: {latitude:13.0517102 ,longitude:80.1899955}
         }
-        // return  {
-        //   canQuantity: action.value.defaultCanOption.quantity,
-        //   canUnit: action.value.defaultCanOption.unit,
-        //   canCount: action.value.defaultCanOption.defaultNumber,
-        //   emergency: action.value.emergencyBooking,
-        //   upperBound: action.value.upperBound,
-        //   lowerBound: action.value.lowerBound,
-        //   currentUserLocation: {latitude:13.0517102 ,longitude:80.1899955}
-        // }
         case 'CURRENT_LOCATION':
         return Object.assign({}, state, {
           currentUserLocation: {latitude: action.latitude,longitude: action.longitude}
