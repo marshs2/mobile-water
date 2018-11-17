d9512c9f84f01f5971de79a9c8feb5f1c7d06b9c
Nirmalkumar Selvaraj <nirmalkumar.s@accelfrontline.com>
2018-11-17 13:31:12 +0530

    feat:agency selection ui

--- MyWaterCanApp/components/AgencySelectionModule/AgencySelectionModule.js
+++ MyWaterCanApp/components/AgencySelectionModule/AgencySelectionModule.js
@@ -1,13 +1,8 @@
 import React, { Component } from 'react';
-import {
-  Platform,
-  StyleSheet,
- TouchableOpacity,
-  View,
-  Text
-  } from 'react-native';
-  import { connect } from 'react-redux';
-  import {screens} from '../../constants/constants'
+import {colors,fontWeight} from '../../constants/helpers';
+import {Platform,Image, StyleSheet, TouchableOpacity,Text, View,AsyncStorage,FlatList,AppState} from 'react-native';
+import { connect } from 'react-redux';
+import {screens} from '../../constants/constants'
 
 
 class AgencySelectionModule extends Component {
@@ -23,10 +18,35 @@ class AgencySelectionModule extends Component {
       animationType: 'slide-up', // 'none' / 'slide-up' , appear animation for the modal (optional, default 'slide-up')
     }); 
   }
+  separator() {
+    return (
+        <View style = {{backgroundColor: colors.separator, height: 1,width:'100%'}}/>
+    )
+  }
+  agencyCell() {
+    return (
+    <View style = {styles.AgencyOutterBox} >
+          <View style = {styles.jobContainer}>
+            <Text style = {styles.jobTitle}>K.AMbalAgency(A711)</Text>
+            <Text style = {styles.jobStatus}>Status: Assigned</Text>
+            <Text style = {styles.jobStatus}>Status: Assigned</Text>
+            <Text style = {styles.jobStatus}>Status: Assigned</Text>
+          </View>
+        </View>
+    )
+  }
   render() {
     return (
     <View style =  {styles.container}>
-     <Text>"AgencySelectionModule"</Text>
+      <View style = {styles.searchContainer}>
+      </View>
+      <View style = {styles.iamfeelinglucky}>
+      </View>
+      <FlatList data={this.props.agencies} showsVerticalScrollIndicator={false} 
+            renderItem={({item}) =>
+            this.agencyCell()
+            }
+            />
      </View>
     );
   }
@@ -38,6 +58,21 @@ const mapDispatchToProps = (dispatch) => {
 };
 function mapStateToProps(state) {
   return {
+    agencies: [ {
+      agencyName:"K.ambalAgency",
+      id:"A711",
+      name: "arumugam",
+      canCost: 35,
+      distance:"1.5 km from your place"
+    },
+    {
+      agencyName:"K.ambalAgency",
+      id:"A711",
+      name: "arumugam",
+      canCost: 35,
+      distance:"1.5 km from your place"
+    }
+    ]
   };
 }
 export default connect(mapStateToProps, mapDispatchToProps)(AgencySelectionModule);
@@ -47,5 +82,116 @@ const styles = StyleSheet.create({
     alignItems: 'stretch',
     height: '100%',
     width: '100%'
+  },
+  searchContainer: {
+    height: 70,
+    width: '100%',
+    backgroundColor: 'red'
+  },
+  iamfeelinglucky: {
+    height: 50,
+    width: '100%',
+    backgroundColor: 'blue'
+  },
+  overlay: {
+    flex: 1,
+    backgroundColor: 'rgba(0, 0, 0, 0.34)',
+    justifyContent: 'space-around',
+  },
+
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+  AgencyOutterBox: {
+    backgroundColor: 'white',
+    margin:12,
+    borderRadius:3,
+    shadowOffset: {
+      width: 0,
+      height: 2
+    },
+    shadowRadius: 5,
+    shadowOpacity: 0.4,
+    shadowColor: 'black'
+  },
+  jobOuterPaddingBox: {
+    backgroundColor: '#f8f9f9'
+  },
+  jobTitle: {
+    fontSize: 20,
+    fontWeight:  "bold",
+    justifyContent: 'center',
+    alignItems: 'center',
+    textAlign: 'center',
+    marginTop: 16,
+    marginLeft: 16,
+    marginRight: 16,
+    marginBottom: 8
+  },
+  jobStatus: {
+    fontSize: 14,
+    fontWeight: fontWeight.Semibold,
+    marginLeft: 16,
+    marginRight: 16,
+    marginBottom: 8,
+    color: 'rgba(0, 0, 0, 0.54)',
+  },
+  jobAddressContainer: {
+    flexDirection: 'row'
+  },
+  jobTimeContainer: {
+    flexDirection: 'row'
+  },
+  jobAddress: {
+    width: '82%'
+  },
+  jobAddressPoint: {
+    alignItems: 'center',
+    width: '18%'
+  },
+  jobTime: {
+    width: '18%',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  jobAddressText: {
+    fontSize: 14,
+    fontWeight: fontWeight.Regular,
+    color: 'rgba(0, 0, 0, 0.87)',
+    marginTop: 14,
+    marginBottom: 14
+  },
+  viewDetailsText: {
+    fontSize: 14,
+    fontWeight: fontWeight.Semibold,
+    color: colors.nccBlue,
+    marginTop: 16,
+    marginBottom: 16,
+    marginLeft: 16
+  },
+  timeImage: {
+    width: 24,
+    height: 24
+  },
+  blueCircularView:{
+    backgroundColor: colors.nccBlue,
+    width:8,
+    height:8,
+    borderRadius: 4,
+    marginTop: 20
+  },
+  blackLine:{
+    flex: 6,
+    backgroundColor: 'rgba(0, 0, 0, 0.12)',
+    width: 1
+  },
+  blackCircularView:{
+    backgroundColor: 'black',
+    width:8,
+    height:8,
+    borderRadius: 4,
+    marginBottom: 20
   }
 });
---
+++ MyWaterCanApp/constants/helpers.js
@@ -0,0 +1,26 @@
+
+
+export const colors = {
+nccLightGray: 'rgba(229, 232, 232, 1.0)',
+nccGray: 'rgba(0, 0, 0, 0.26)',
+nccBlue: 'rgba(0, 107, 230, 1.0)',
+nccRed: 'rgba(214, 0, 0, 1.0)',
+separator: 'rgba(0, 0, 0, 0.12)'
+}
+export const fontWeight = {
+Thin: '100',
+UltraLight: '200',
+Light: '300', 
+Regular: '400',
+Medium: '500',
+Semibold: '600',
+Bold: '700',
+Heavy: '800',
+Black: '900'
+}
+
+
+export const textFieldType = {
+    email: 'email',
+    password: 'password'
+}
